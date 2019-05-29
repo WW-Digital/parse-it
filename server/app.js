7f1d57fb1f93e6f9f2e9aad5e61a687003eb459f
Pat Cedrowski <pcedrowski@gmail.com>
2019-05-29 15:36:38 -0400

    Removed some files :)

--- server/app.js
+++ server/app.js
@@ -3,25 +3,3 @@ const express = require('express');
 const app = express();
 
 //TODO: Add an express router
-
-app.get('/', function (req, res) {
-    res.send('Parse It!');
-});
-
-//TODO Fulfill endpoint to list Identifiers & Stop Words
-app.get('/mealtimes', function (req, res) {
-    res.json({todo: 'List mealtimes'});
-});
-app.get('/dates', function (req, res) {
-    res.json({todo: 'List dates'});
-});
-
-//TODO Parse the query!
-app.post('/parse', function (req, res) {
-    const query = req.param('query');
-    res.json({todo: `Parse the query string: ${query}`});
-});
-
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
-});
